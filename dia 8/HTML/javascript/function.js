ad8faa7bb804f8aad9ef90229f8f859b58e09659
Jorge Correa <42034339+yosoyjacko@users.noreply.github.com>
2019-06-10 15:24:12 -0300

    Add files via upload

---
+++ dia 8/HTML/javascript/function.js
@@ -0,0 +1,67 @@
+console.log("hello world!")
+
+const canvas = document.querySelector('#draw');
+
+const ctx = canvas.getContext('2d');
+
+//imagen para fondo
+var img = new Image ();
+img.src = "img/gow.jpg";
+ctx.drawImage(img, 0, 0);
+img.onload = function(){
+    ctx.drawImage(img, 0,0);
+}
+//termina el codigo para la imagen de fondo
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = '100';
+// buscar en google 'global composite operator' 
+ctx.globalCompositeOperation = 'screen';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0
+let direction = true;
+
+function draw(e) {
+    if (!isDrawing) return; //detiene la funcion cuando no se cliakea en el mouse
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if(hue >= 360) {
+        hue = 0
+    }
+
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+        direction = !direction;
+    } 
+    if(direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
